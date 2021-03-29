4c07ee8eb89774a3e98e225376a64f7c04abe99f
SebaGaude <gaudellisebastian@yahoo.com>
2021-03-29 12:54:52 -0300

    solucion de conflicto-cambio en el archivo6.js

--- archivo6.js
+++ archivo6.js
@@ -10,10 +10,6 @@ function saludar (nombre, apellido, callback){
 function despedir (nombre, apellido, callback){
     return "Chau, " + callback(nombre, apellido);    
 }
-<<<<<<< HEAD
 console.log(saludar("Juan Carlos", "Gomez", unirNombres));
-=======
-console.log(saludar("Juan Pedro", "Gomez", unirNombres));
->>>>>>> 3741e8dd26755bb76a7dcb27417b5d56919b528c
 
 console.log(despedir("Maria Luisa", "Gimenez", unirNombres));
